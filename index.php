be9881372802ebd98a3010e27f6563239dfe5017
guillaume <guillaumechaulet@hotmail.fr>
2020-05-15 15:55:35 +0200

    array part 1

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'vendor/autoload.php';
+$pdo= new PDO("sqlite:./products.db", null, null, [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+$products = $pdo->query("SELECT * FROM products LIMIT 20")->fetchAll();
+
+//dd($products);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Real Estate</title>
+</html>
+
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>City</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($products as $product): ?>
+            <tr>
+                <td>#<?= $product['id'] ?></td>
+                <td><?= $product['name'] ?></td>
+                <td><?= Number::price($product['price']); ?></td>
+                <td><?= $product['city'] ?></td>
+                <td><?= $product['address'] ?></td>
+            </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</body>
