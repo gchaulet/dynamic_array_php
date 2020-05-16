af20ba40a1ea5c5aeacc16144e40bd0fdeb3d1b0
guillaume <guillaumechaulet@hotmail.fr>
2020-05-16 10:19:17 +0200

    array sort indication inside title  part 7

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dynamic_array_php
+
+#A form gives the way to filter his search by name, price, city, address
+
+#Organize and filter a search with parameters
+
+#Those filter changes are shown in url
+
--- src/TableHelper.php
+++ src/TableHelper.php
@@ -8,11 +8,21 @@ class TableHelper {
 
     public static function sort(string $sortKey, string $label, array $data): string
     {
+
         $sort = $data[self::SORT_KEY] ?? null;
         $direction = $data [self::DIR_KEY] ?? null;
-        $url = URLHelper::withParams($data, ['sort' => $sortKey, 'dir' => 'asc']);
+        $icon ="";
+        //show sort in active title
+        if($sort === $sortKey){
+            $icon =  $direction === 'asc' ? "^" : 'v';
+        }
+        //show direction in url and initialize sort in active title only
+        $url = URLHelper::withParams($data, [
+            'sort' => $sortKey, 
+            'dir' => $direction === 'asc' && $sort === $sortKey ? "desc" : 'asc'
+        ]);
         return <<<HTML
-        <a href="?$url">$label</a>
+        <a href="?$url">$label $icon</a>
         HTML;
     }
 }
